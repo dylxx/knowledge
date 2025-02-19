3cafc6df80f94afa553962f1deed7ce14596a7fc
dylxx <15125099021@163.com>
2025-02-20 01:51:51 +0800

    基础样式

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+      <FloatingBall />
+    </div>
+  </template>
+  
+  <script>
+  // 导入悬浮球组件
+  import FloatingBall from './components/FloatingBall.vue';
+  
+  export default {
+    name: 'App',
+    components: {
+      FloatingBall,
+    },
+  };
+  </script>
+  
+<style>
+#app {
+    text-align: center;
+}
+</style>
